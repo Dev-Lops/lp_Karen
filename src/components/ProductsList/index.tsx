401205fd4e0f3ea37b15cbaefba1daf9c77c874c
anderson da silva lopes <developesanderson@gmail.com>
2024-11-18 11:32:52 -0400

    ✅ Update a news features - update a img products list

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -3,6 +3,16 @@
 import { Section, Container, CardWrapper, CardContent } from "./styles"
 import { CircleArrowOutUpRight, CircleSlash2 } from "lucide-react"
 
+import img1 from "../../assets/IMG_8098.jpg"
+import img2 from "../../assets/serun.jpg"
+import img3 from "../../assets/gloss_natual_e_clean_detox.jpg"
+import img4 from "../../assets/mascara.jpg"
+import img5 from "../../assets/escova.jpg"
+import img6 from "../../assets/perfume capilar.jpg"
+import img7 from "../../assets/reestruturador_bifasico.jpg"
+import img8 from "../../assets/restaurador cmc.jpg"
+import img9 from "../../assets/shampoo.jpg"
+
 interface Product {
   id: number
   image: string
@@ -14,63 +24,63 @@ interface Product {
 const products: Product[] = [
   {
     id: 1,
-    image: "src/assets/IMG_8098.jpg",
+    image: img1,
     name: "Ampola de Reestruturação do Cortex",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Ampola%20de%20Reestruturação%20do%20Cortex",
     isOutOfStock: false,
   },
   {
     id: 2,
-    image: "src/assets/serun.jpg",
+    image: img2,
     name: "Serum Revitalizante",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Serum%20Revitalizante",
     isOutOfStock: false,
   },
   {
     id: 3,
-    image: "src/assets/gloss_natual_e_clean_detox.jpg",
+    image: img3,
     name: "Kit Alisamento Natural",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Kit%20Alisamento%20Natural",
     isOutOfStock: false,
   },
   {
     id: 4,
-    image: "src/assets/mascara.jpg",
+    image: img4,
     name: "Mascara Equilíbrio THERAPY",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Mascara%20Equilíbrio%20THERAPY",
     isOutOfStock: false,
   },
   {
     id: 5,
-    image: "src/assets/escova.jpg",
+    image: img5,
     name: "Escova de Cabelo",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Escova%20de%20Cabelo",
     isOutOfStock: false,
   },
   {
     id: 6,
-    image: "src/assets/perfume capilar.jpg",
+    image: img6,
     name: "Perfume Capilar",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Perfume%20Capilar",
     isOutOfStock: false,
   },
   {
     id: 7,
-    image: "src/assets/reestruturador_bifasico.jpg",
+    image: img7,
     name: "Reestruturador bifásico Equilíbrio THERAPY",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Reestruturador%20bifásico%20Equilíbrio%20THERAPY",
     isOutOfStock: true,
   },
   {
     id: 8,
-    image: "src/assets/restaurador cmc.jpg",
+    image: img8,
     name: "Restaurador do CMC Capilar",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Restaurador%20do%20CMC%20Capilar",
     isOutOfStock: false,
   },
   {
     id: 9,
-    image: "src/assets/shampoo.jpg",
+    image: img9,
     name: "Shampoo Equilíbrio THERAPY",
     link: "https://wa.me/5592993787566?text=Olá,%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20produto%20Shampoo%20Equilíbrio%20THERAPY",
     isOutOfStock: false,
