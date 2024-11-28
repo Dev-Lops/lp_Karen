8ba6e89d5be51b66a2af5eb519b8e971a3c2ca8a
anderson da silva lopes <developesanderson@gmail.com>
2024-11-28 09:13:32 -0400

    ✅ Update a news features -  lazy loading images testemonials

--- src/components/TestimonialCarousel/index.tsx
+++ src/components/TestimonialCarousel/index.tsx
@@ -1,8 +1,7 @@
 // Importando o Slider do react-slick
 import "slick-carousel/slick/slick.css" // Estilos do slick
 import "slick-carousel/slick/slick-theme.css" // Estilos do tema do slick
-import {  TestimonialCard } from "./styles"
-import LazyImage from '../LazyImg'
+import { ClientImage, TestimonialCard } from "./styles"
 
 interface TestimonialProps {
   image: string
@@ -10,6 +9,6 @@ interface TestimonialProps {
 
 export const Testimonial = ({ image }: TestimonialProps) => (
   <TestimonialCard data-aos='fade-up' data-aos-duration='3000'>
-    <LazyImage src={image} alt='' />
+    <ClientImage src={image} alt='' />
   </TestimonialCard>
 )
